89dbfd88627a39e7bcc8755fe9498d6b87bccabb
Ashraf840 <shift2unleashed2011@gmail.com>
2024-01-06 05:45:30 +0600

    Made a list view page of Products table data (Mistakenly skipped adding the new updates).

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,6 +1,6 @@
 from django.views import generic
 
-from product.models import Variant
+from product.models import Variant, Product
 
 
 class CreateProductView(generic.TemplateView):
@@ -11,4 +11,15 @@ class CreateProductView(generic.TemplateView):
         variants = Variant.objects.filter(active=True).values('id', 'title')
         context['product'] = True
         context['variants'] = list(variants.all())
+        return context
+
+class ListProductView(generic.TemplateView):
+    template_name = 'products/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product_list = Product.objects.all()
+        context['products'] = product_list
+        # for prod in product_list:
+        #     print(prod)
         return context
