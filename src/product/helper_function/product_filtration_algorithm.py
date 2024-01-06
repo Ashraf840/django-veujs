f5248dd3873e31e7938679c52d9fcf2a5683b838
Ashraf840 <shift2unleashed2011@gmail.com>
2024-01-07 01:55:21 +0600

    Completed the filtration algorithm with lastly all inputs for querying

--- src/product/helper_function/product_filtration_algorithm.py
+++ src/product/helper_function/product_filtration_algorithm.py
@@ -250,4 +250,116 @@ def filtration_cursor(**kwargs) -> tuple():
                 or kwargs['price_to'] != None) \
                 and kwargs['product_enlisted_after_date'] != None:
                     print("Make query with product_title, (price_from or price_to) & date!")
+                    if kwargs['product_title'] != None and kwargs['price_from'] != None and kwargs['price_to'] == None and kwargs['product_enlisted_after_date'] != None:
+                        print("Query: product_title, price_from & product_enlisted_after_date!")
+                        return (
+                            (Q(productvariantprice__product__title__icontains=kwargs['product_title']) if kwargs['product_title'] else Q())
+                            & (Q(productvariantprice__price__gte=kwargs['price_from']) if kwargs['price_from'] else Q())
+                            & (Q(productvariantprice__product__created_at__gte=kwargs['product_enlisted_after_date']) if kwargs['product_enlisted_after_date'] else Q())
+                        )
+                    if kwargs['product_title'] != None and kwargs['price_from'] == None and kwargs['price_to'] != None and kwargs['product_enlisted_after_date'] != None:
+                        print("Query: product_title, price_to & product_enlisted_after_date!")
+                        return (
+                            (Q(productvariantprice__product__title__icontains=kwargs['product_title']) if kwargs['product_title'] else Q())
+                            & (Q(productvariantprice__price__lte=kwargs['price_to']) if kwargs['price_to'] else Q())
+                            & (Q(productvariantprice__product__created_at__gte=kwargs['product_enlisted_after_date']) if kwargs['product_enlisted_after_date'] else Q())
+                        )
+                    if kwargs['product_title'] != None and kwargs['price_from'] != None and kwargs['price_to'] != None and kwargs['product_enlisted_after_date'] != None:
+                        print("Query: product_title, price_from, price_to & product_enlisted_after_date!")
+                        return (
+                            (Q(productvariantprice__product__title__icontains=kwargs['product_title']) if kwargs['product_title'] else Q())
+                            & (Q(productvariantprice__price__gte=kwargs['price_from']) if kwargs['price_from'] else Q())
+                            & (Q(productvariantprice__price__lte=kwargs['price_to']) if kwargs['price_to'] else Q())
+                            & (Q(productvariantprice__product__created_at__gte=kwargs['product_enlisted_after_date']) if kwargs['product_enlisted_after_date'] else Q())
+                        )
+            
+            # Query: variant, price & date
+            if kwargs['product_title'] == None \
+                and kwargs['variant_id'] != None \
+                and (kwargs['price_from'] != None \
+                or kwargs['price_to'] != None) \
+                and kwargs['product_enlisted_after_date'] != None:
+                    print("Make query with variant, (price_from or price_to) & date!")
+                    if kwargs['variant_id'] != None and kwargs['price_from'] != None and kwargs['price_to'] == None and kwargs['product_enlisted_after_date'] != None:
+                        print("Query: variant, price_from & product_enlisted_after_date!")
+                        return (
+                            ((Q(productvariantprice__product_variant_one__variant__pk=kwargs['variant_id']) 
+                              | Q(productvariantprice__product_variant_two__variant__pk=kwargs['variant_id']) 
+                              | Q(productvariantprice__product_variant_three__variant__pk=kwargs['variant_id'])) if kwargs['variant_id'] else Q())
+                            & (Q(productvariantprice__price__gte=kwargs['price_from']) if kwargs['price_from'] else Q())
+                            & (Q(productvariantprice__product__created_at__gte=kwargs['product_enlisted_after_date']) if kwargs['product_enlisted_after_date'] else Q())
+                        )
+                    if kwargs['variant_id'] != None and kwargs['price_from'] == None and kwargs['price_to'] != None and kwargs['product_enlisted_after_date'] != None:
+                        print("Query: variant, price_to & product_enlisted_after_date!")
+                        return (
+                            ((Q(productvariantprice__product_variant_one__variant__pk=kwargs['variant_id']) 
+                              | Q(productvariantprice__product_variant_two__variant__pk=kwargs['variant_id']) 
+                              | Q(productvariantprice__product_variant_three__variant__pk=kwargs['variant_id'])) if kwargs['variant_id'] else Q())
+                            & (Q(productvariantprice__price__lte=kwargs['price_to']) if kwargs['price_to'] else Q())
+                            & (Q(productvariantprice__product__created_at__gte=kwargs['product_enlisted_after_date']) if kwargs['product_enlisted_after_date'] else Q())
+                        )
+                    if kwargs['variant_id'] != None and kwargs['price_from'] != None and kwargs['price_to'] != None and kwargs['product_enlisted_after_date'] != None:
+                        print("Query: variant, price_from, price_to & product_enlisted_after_date!")
+                        return (
+                            ((Q(productvariantprice__product_variant_one__variant__pk=kwargs['variant_id']) 
+                              | Q(productvariantprice__product_variant_two__variant__pk=kwargs['variant_id']) 
+                              | Q(productvariantprice__product_variant_three__variant__pk=kwargs['variant_id'])) if kwargs['variant_id'] else Q())
+                            & (Q(productvariantprice__price__gte=kwargs['price_from']) if kwargs['price_from'] else Q())
+                            & (Q(productvariantprice__price__lte=kwargs['price_to']) if kwargs['price_to'] else Q())
+                            & (Q(productvariantprice__product__created_at__gte=kwargs['product_enlisted_after_date']) if kwargs['product_enlisted_after_date'] else Q())
+                        )
+
+            # Input Count: 4
+            if kwargs['product_title'] != None \
+                and kwargs['variant_id'] != None \
+                and (kwargs['price_from'] != None \
+                or kwargs['price_to'] != None) \
+                and kwargs['product_enlisted_after_date'] != None:
+                    print("Make query with all the inputs product_title, variant, (price_from or price_to) & date!")
+                    if kwargs['product_title'] != None \
+                        and kwargs['variant_id'] != None \
+                        and kwargs['price_from'] != None \
+                        and kwargs['price_to'] == None \
+                        and kwargs['product_enlisted_after_date'] != None:
+                            print("Query: product_title, variant, price_from & product_enlisted_after_date!")
+                            return (
+                                (Q(productvariantprice__product__title__icontains=kwargs['product_title']) if kwargs['product_title'] else Q())
+                                &((Q(productvariantprice__product_variant_one__variant__pk=kwargs['variant_id']) 
+                                    | Q(productvariantprice__product_variant_two__variant__pk=kwargs['variant_id']) 
+                                    | Q(productvariantprice__product_variant_three__variant__pk=kwargs['variant_id'])) if kwargs['variant_id'] else Q())
+                                & (Q(productvariantprice__price__gte=kwargs['price_from']) if kwargs['price_from'] else Q())
+                                & (Q(productvariantprice__product__created_at__gte=kwargs['product_enlisted_after_date']) if kwargs['product_enlisted_after_date'] else Q())
+                            )
+                    if kwargs['product_title'] != None \
+                        and kwargs['variant_id'] != None \
+                        and kwargs['price_from'] == None \
+                        and kwargs['price_to'] != None \
+                        and kwargs['product_enlisted_after_date'] != None:
+                            print("Query: product_title, variant, price_to & product_enlisted_after_date!")
+                            return (
+                                (Q(productvariantprice__product__title__icontains=kwargs['product_title']) if kwargs['product_title'] else Q())
+                                &((Q(productvariantprice__product_variant_one__variant__pk=kwargs['variant_id']) 
+                                    | Q(productvariantprice__product_variant_two__variant__pk=kwargs['variant_id']) 
+                                    | Q(productvariantprice__product_variant_three__variant__pk=kwargs['variant_id'])) if kwargs['variant_id'] else Q())
+                                & (Q(productvariantprice__price__lte=kwargs['price_to']) if kwargs['price_to'] else Q())
+                                & (Q(productvariantprice__product__created_at__gte=kwargs['product_enlisted_after_date']) if kwargs['product_enlisted_after_date'] else Q())
+                            )
+                    if kwargs['product_title'] != None \
+                        and kwargs['variant_id'] != None \
+                        and kwargs['price_from'] != None \
+                        and kwargs['price_to'] != None \
+                        and kwargs['product_enlisted_after_date'] != None:
+                            print("Query: product_title, variant, price_from, price_to & product_enlisted_after_date!")
+                            return (
+                                (Q(productvariantprice__product__title__icontains=kwargs['product_title']) if kwargs['product_title'] else Q())
+                                &((Q(productvariantprice__product_variant_one__variant__pk=kwargs['variant_id']) 
+                                    | Q(productvariantprice__product_variant_two__variant__pk=kwargs['variant_id']) 
+                                    | Q(productvariantprice__product_variant_three__variant__pk=kwargs['variant_id'])) if kwargs['variant_id'] else Q())
+                                & (Q(productvariantprice__price__gte=kwargs['price_from']) if kwargs['price_from'] else Q())
+                                & (Q(productvariantprice__price__lte=kwargs['price_to']) if kwargs['price_to'] else Q())
+                                & (Q(productvariantprice__product__created_at__gte=kwargs['product_enlisted_after_date']) if kwargs['product_enlisted_after_date'] else Q())
+                            )
+                            
+
+                        
             
